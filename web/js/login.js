097aad3cbec1072de4fec9b56bde8ade495431b0
nikorova <nick@korovasoft.com>
2012-06-26 22:54:40 -0400

    examine school_data object

--- web/js/login.js
+++ web/js/login.js
@@ -105,8 +105,6 @@ function generateSchoolList(response) {
         alert(err);
     }
     
-   var test_h3 = $("h3#test_schools").append(school_data["2"]); 
-   test_h3.append(school_data["5"]);
 
     var schools_html = [];
 
@@ -129,6 +127,8 @@ function generateSchoolList(response) {
                 
                 console.log(index);
                 console.dir(school);
+           var test_h3 = $("h3#test_schools").append(school_data["2"]); 
+           test_h3.append(school_data["5"]);
 
                 editSchoolEntity(school);
             });
