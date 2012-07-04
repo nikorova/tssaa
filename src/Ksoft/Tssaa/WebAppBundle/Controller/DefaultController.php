69fa3ef7635a8360a306673bac806b40e6642f0f
nikorova <nick@korovasoft.com>
2012-07-04 16:18:21 -0500

    testing data transfer type in backend

--- src/Ksoft/Tssaa/WebAppBundle/Controller/DefaultController.php
+++ src/Ksoft/Tssaa/WebAppBundle/Controller/DefaultController.php
@@ -9,11 +9,13 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 class DefaultController extends Controller
 {
    	/**
-	 * @Route("index/{name}", requirements={"_method" = "GET"}, defaults={"_format" = "json"}) 
+	 * @Route("index/", requirements={"_method" = "POST"}, defaults={"_format" = "json"}) 
 	 */
     public function indexAction($name)
     {
-#        return $this->render('KsoftTssaaLoginBundle:Default:index.html.twig', array('name' => $name));
-        return new Response(json_encode( array('name' => $name)));
+		$req = $this->getRequest();
+
+		$content = $req->getContent();	
+        return new Response($content);
     }
 }
