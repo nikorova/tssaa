0c9e059ca0c4bc4766db32c3d50a84da98f248a3
nikorova <nick@korovasoft.com>
2012-07-05 20:29:03 -0500

    handle json format in login controller

--- src/Ksoft/Tssaa/WebAppBundle/Controller/LoginController.php
+++ src/Ksoft/Tssaa/WebAppBundle/Controller/LoginController.php
@@ -12,23 +12,49 @@ class LoginController extends Controller {
 	 * @Route("login"), requirements={"_method" = "POST"}
 	 */
 	public function loginAction(){
-		// fire up the logger
 		$logger = $this->get('logger');
 
-		// get login info
-		$req = Request::createFromGlobals();
-		$login = $req->request->get('login');
-		$pass = $req->request->get('pass');
+		// decode json from request
+		$req = $this->getRequest();
+		$content = array();
+		try {
+			$content = json_decode($req->getContent(), true);
+		} catch (Exception $err) {
+			$err_response = array(
+				"status" => "failure",
+				"exception" => $err,
+				"payload" => NULL,
+			);
+			return new Response(json_encode($err_response));
+		}
+			
+		$login = $content["login"];
+		$pass = $content["pass"]; 
+
+		// log login attempts
 		$logger->debug("Recieved login request:\n  Params:\n  login: $login\n  pass: $pass\n");
 		
-	  	
 		// check against members table
 		$repo = $this->getDoctrine()->getRepository('WebAppBundle:Member');
-		if ($member = $repo->findOneBy(array('login' => $login, 'pass' => $pass))){
-			return new Response(json_encode(array('response' => 'success', 'login' => $login, 'pass' => $pass)));
+
+		$status = NULL;
+		$exception = NULL;
+		$payload = NULL;
+		
+		if ($result = $repo->findOneBy(array('login' => $login, 'pass' => $pass))){
+			$status = "success";
+			$payload = $result;
 		} else { 
-			return new Response(json_encode(array('response' => 'denied', 'login' => $login, 'pass' => $pass)));
+			$status = "failure";
+			$exception = $result;
 		}
-	
+
+		$login_response = array(
+			"status" => $status,
+			"exception" => empty($exception) ? NULL : $exception,
+			"payload" => empty($payload) ?  NULL : $payload,
+		);
+
+		return new Response(json_encode($login_response));		
 	}
 }
