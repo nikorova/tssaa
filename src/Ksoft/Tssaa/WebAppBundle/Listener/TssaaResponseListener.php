a6d3e0101443ad8c0ed16c88c960cc4bf1279812
nikorova <nick@korovasoft.com>
2012-07-09 16:36:34 -0500

    resp listener encodes according to template

--- src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaResponseListener.php
+++ src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaResponseListener.php
@@ -6,7 +6,11 @@ use Symfony\Component\HttpFoundation\Response;
 
 class TssaaResponseListener {
 	public function onKernelResponse (GetResponseForControllerResultEvent $event) {
-		$server_data = $event->getControllerResult();
+		$server_data = array(
+			"status" => "success",
+			"exception" => NULL,
+			"payload" => $event->getControllerResult(),
+		);
 
 		$response = new Response(json_encode($server_data));
 
