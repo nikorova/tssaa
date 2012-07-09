978048f6527114135e6c66e4415bd1e6a592ea80
nikorova <nick@korovasoft.com>
2012-07-09 12:15:17 -0500

    let decode exception bubble from ReqListen

---
+++ src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaRequestListener.php
@@ -0,0 +1,24 @@
+<?php
+// src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaRequestListener.php
+namespace Ksoft\Tssaa\WebAppBundle\Listener;
+
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\HttpKernel\Request;
+
+class TssaaRequestListener {
+	public function onKernelRequest(GetResponseEvent $event) {
+		// get client json string from incoming Request
+		$req = $event->getRequest();
+		$req_data = $request->getContent();
+
+		// Letting this exception bubble up
+		$decoded = json_decode($req_data, true);
+
+		// stick decoded and arrayified client data in Request's
+		// parameter bag
+		$p_bag = $req->request();
+		$p_bag->set("client_data", $decoded);	
+		}
+	}
+}	
+?>
