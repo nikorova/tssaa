cfdd445395b6beb5e34781f952ac21832deba270
nikorova <nick@korovasoft.com>
2012-07-09 20:13:26 -0500

    get exception type for response object

--- src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaExceptionListener.php
+++ src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaExceptionListener.php
@@ -1,19 +1,27 @@
 <?php
 namespace Ksoft\Tssaa\WebAppBundle\Listener;
 
-use Symfony\Component\HttpKernel\Event\GetResponseFromExceptionEvent;
+use Symfony\Component\HttpKernel\Event\GetResponseForExceptionEvent;
 use Symfony\Component\HttpFoundation\Response;
 
 class TssaaExceptionListener {
-	public function onKernelException(GetResponseFromExceptionEvent $event) {
+	public function onKernelException(GetResponseForExceptionEvent $event) {
 		$exception = $event->getException();
 		
-		$exception_response = array(
+		// build our exception flavored response object
+		$exception_array = array(
 			"status" => "failure",
-			"exception" => $exception,
+			"exception" => array(
+				"message" 		=> $exception->getMessage(),
+				"code" 			=> $exception->getCode(),
+				"stack_trace" 	=> $exception->getTraceAsString(),
+				"type" 			=> $exception->get_class($exception),
+			),
 		   	"payload" => NULL,	
 		);	
-
-		return new Response(json_encode($exception_response));
+		
+		// encode array a la json, make new Response of it
+		$exception_response = new Response(json_encode($exception_array));
+		$event->setResponse($exception_response);
 	}
 }
