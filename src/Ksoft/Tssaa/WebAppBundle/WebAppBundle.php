ed7470d1e3d39d4e957b6ae4dcc5c6601acfd4c0
nikorova <nick@korovasoft.com>
2012-07-13 14:51:53 -0500

    define auth services and add sec factory to bundle

--- src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
+++ src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
@@ -2,8 +2,15 @@
 
 namespace Ksoft\Tssaa\WebAppBundle;
 
+use Ksoft\Tssaa\WebAppBundle\Security\Factory\WsseFactory;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
-class WebAppBundle extends Bundle
-{
+class WebAppBundle extends Bundle {
+	public function build(ContainerBuilder $container) {
+		parent::build($container);
+
+		$extension = container->getExtension('security');
+		$extension->addSecurityListenerFactory(new WsseFactory());
+	}
 }
