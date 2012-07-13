8a7d02e681a19ff288ac49234dfa5798e16d4a7c
nikorova <nick@korovasoft.com>
2012-07-13 15:15:56 -0500

    try using SecurityExtension class directly

--- src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
+++ src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
@@ -3,6 +3,7 @@
 namespace Ksoft\Tssaa\WebAppBundle;
 
 use Ksoft\Tssaa\WebAppBundle\Security\Factory\WsseFactory;
+use Symfony\Bundle\SecurityBundle\DependencyInjection\SecurityExtension;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
