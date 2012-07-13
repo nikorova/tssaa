26457a15f93e639ddbda35f2244d5050dcf734d4
nikorova <nick@korovasoft.com>
2012-07-13 11:24:13 -0500

    wsse token class

---
+++ src/Ksoft/Tssaa/WebAppBundle/Security/Authentication/Token/WsseUserToken.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Ksoft\Tssaa\WebAppBundle\Security\Authentication\Token;
+
+use Symfony\Component\Security\Core\Authentication\Token\AbstractToken;
+
+class WsseUserToken extends AbstractToken {
+	public $created;
+	public $digest;
+	public $nonce;
+
+	public function __construct(array $roles = array()) {
+		parent::__construct($roles);
+
+		$this->setAuthenticated(count($roles) > 0);
+	}
+
+	public function getCredentials() {
+		return '';
+	}
+}
