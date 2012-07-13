c9d1c404be517b981db2921550305c21de19328c
nikorova <nick@korovasoft.com>
2012-07-13 17:03:09 -0500

    version 2.0 requires factory in config

--- src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
+++ src/Ksoft/Tssaa/WebAppBundle/WebAppBundle.php
@@ -8,10 +8,4 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class WebAppBundle extends Bundle {
-	public function build(ContainerBuilder $container) {
-		parent::build($container);
-
-		$extension = $container->getExtension('security');
-		$extension->addSecurityListenerFactory(new WsseFactory());
-	}
 }
