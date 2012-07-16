4e5a6d2d39a01403f835fc09f5e147df75d2b9c6
nikorova <nick@korovasoft.com>
2012-07-16 13:46:58 -0500

    fix use statement to refer accuratly to namespace

--- src/Ksoft/Tssaa/WebAppBundle/Security/Firewall/WsseListener.php
+++ src/Ksoft/Tssaa/WebAppBundle/Security/Firewall/WsseListener.php
@@ -6,7 +6,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\Security\Http\Firewall\ListenerInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
-use Symfony\Component\Security\SecurityContextInterface;
+use Symfony\Component\Security\Core\SecurityContextInterface;
 use Symfony\Component\Security\Core\Authentication\AuthenticationManagerInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Ksoft\Tssaa\WebAppBundle\Security\Authentication\Token\WsseUserToken;
