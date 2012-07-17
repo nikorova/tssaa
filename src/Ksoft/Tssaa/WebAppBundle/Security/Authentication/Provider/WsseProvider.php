3721517acdf60dcebde4cb4cf8e17aaa0d197b50
nikorova <nick@korovasoft.com>
2012-07-17 13:19:00 -0500

    remove firephp instance

--- src/Ksoft/Tssaa/WebAppBundle/Security/Authentication/Provider/WsseProvider.php
+++ src/Ksoft/Tssaa/WebAppBundle/Security/Authentication/Provider/WsseProvider.php
@@ -16,8 +16,6 @@ class WsseProvider implements AuthenticationProviderInterface {
 	private $userProvider;
 	private $cacheDir;
 
-	public $firephp = \FirePHP::getInstance(true);
-
 	public function __construct(UserProviderInterface $userProvider, $cacheDir) {
 		$this->userProvider = $userProvider;
 		$this->cacheDir = $cacheDir;
