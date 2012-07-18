bb02daa9cf6c99fd5b62197d2c85d35bd339e023
nikorova <nick@korovasoft.com>
2012-07-18 03:12:12 -0500

    fix reuquire path

--- src/Ksoft/Tssaa/WebAppBundle/Security/Authentication/Provider/WsseProvider.php
+++ src/Ksoft/Tssaa/WebAppBundle/Security/Authentication/Provider/WsseProvider.php
@@ -8,7 +8,7 @@ use Symfony\Component\Security\Core\Exception\NonceExpiredException;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Ksoft\Tssaa\WebAppBundle\Security\Authentication\Token\WsseUserToken;
 
-require_once('firelogger.php');
+require_once('firelogger.php/firelogger.php');
 
 class WsseProvider implements AuthenticationProviderInterface {
 	private $userProvider;
