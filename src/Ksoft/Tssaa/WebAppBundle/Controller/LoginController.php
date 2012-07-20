6cd7882677f55ad73e9e84d902dbb552dad5cab6
nikorova <nick@korovasoft.com>
2012-07-19 19:28:46 -0500

    throw log party

--- src/Ksoft/Tssaa/WebAppBundle/Controller/LoginController.php
+++ src/Ksoft/Tssaa/WebAppBundle/Controller/LoginController.php
@@ -9,12 +9,18 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
 use Symfony\Component\Security\Core\SecurityContextInterface;
 
+require_once('firelogger.php/firelogger.php');
+
 class LoginController extends Controller {
 	/**
 	 * @Route("login"), requirements={"_method" = "POST"}
 	 */
 	public function loginAction(){
+		$fl = new \FireLogger('LoginController');
 		$token = $this->get('security.context')->getToken();
+
+		$fl->info('le token', $token);
+		$fl->info('user obj from token', $token->getUsername());
 
 		return ($token);
 	}
