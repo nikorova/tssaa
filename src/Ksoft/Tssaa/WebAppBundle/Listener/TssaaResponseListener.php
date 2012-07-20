5b1f50d3a17fda85a9bcd5e1bfcfeee50fe6b132
nikorova <nick@korovasoft.com>
2012-07-19 19:39:03 -0500

    log event and result of getControllerRes

--- src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaResponseListener.php
+++ src/Ksoft/Tssaa/WebAppBundle/Listener/TssaaResponseListener.php
@@ -4,14 +4,19 @@ namespace Ksoft\Tssaa\WebAppBundle\Listener;
 use Symfony\Component\HttpKernel\Event\GetResponseforControllerResultEvent;
 use Symfony\Component\HttpFoundation\Response;
 
+require_once('firelogger.php/firelogger.php');
+
 class TssaaResponseListener {
 	public function onKernelResponse (GetResponseForControllerResultEvent $event) {
+		$fl = new \FireLogger('TssaaResponseListener');
+		$fl->log($event);
+
 		$server_data = array(
 			"status" => "success",
 			"exception" => NULL,
-			"payload" => $event->getControllerResult(),
+			"payload" => $fl->log($event->getControllerResult()),
 		);
-
+		
 		$response = new Response(json_encode($server_data));
 
 		$event->setResponse($response);
